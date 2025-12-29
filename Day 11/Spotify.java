f2ca195e59901a812e99c38cc9417702dbf8ea2f
praveen1517-git <praveendsp4503@gmail.com>
2025-12-29 16:43:02 +0530

    homework

---
+++ Day 11/Spotify.java
@@ -0,0 +1,84 @@
+import java.util.Scanner;
+class Spotify
+{
+	public static void main(String[]args)
+	{
+	  Scanner sc = new Scanner(System.in);
+	 
+		System.out.println("Spotify Welcomes You");
+		System.out.println("1.A.R Rahuman");
+		System.out.println("2.Anirudh");
+		System.out.println("3.Harish Jeyaraj");
+		System.out.println("4.U1");
+		System.out.println("5.Illaiyaraja");
+		System.out.println("select your option : ");
+		
+	 int music=sc.nextInt();
+	 if(music==1)
+	 {
+		System.out.println("1.Melody");
+		System.out.println("2.Kuthu");
+		System.out.println("3.Motivation");
+		System.out.println("4.Sad");
+		System.out.println("select your option : ");
+
+		 
+		 
+		 
+	 }
+	 int option=sc.nextInt();
+	 if (option==1)
+	 {
+		 System.out.println("Netru illadha matram");
+		 System.out.println("Enna solla pogirai");
+		 System.out.println("Ennavale");
+		 System.out.println("Malargale");
+		 
+		 
+		 
+		 
+		 
+	 }
+	 else if (option==2)
+	 {
+		System.out.println("Thaaya thaaya");
+		System.out.println("Mukkala mukkapilla");
+		System.out.println("urvasi");
+		System.out.println("aathichoodi");	 
+	 }
+	 else if (option==3)
+	 {
+		System.out.println("Vanthemathuram");
+	    System.out.println("oruvan ");
+		System.out.println("Sakthi");
+		System.out.println("Kappal");
+		
+	}
+	else if (option==4)
+	{
+		System.out.println("Poogatrile");
+		System.out.println("Maruthameni");
+		System.out.println("Rasaathi");
+		System.out.println("Oru poyavathu");
+	}
+	else 
+	{
+		System.out.println("Not Available");
+		
+}
+	
+	
+	
+	
+	
+	
+	
+
+	}
+
+
+
+
+
+
+}
