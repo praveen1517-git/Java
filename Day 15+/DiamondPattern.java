0c9ce3010dfe8c5f6ceaf46a94ff430398b35dfd
praveen1517-git <praveendsp4503@gmail.com>
2026-01-05 09:05:56 +0530

    homework

---
+++ Day 15+/DiamondPattern.java
@@ -0,0 +1,33 @@
+class DiamondPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) 
+		{
+            for (int s = i; s < n; s++) 
+			{
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++) 
+			{
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        
+        for (int i = n - 1; i >= 1; i--) 
+		{
+            for (int s = n; s > i; s--) 
+			{
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= (2 * i - 1); j++)
+				{
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
