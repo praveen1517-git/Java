40dabbdacee858d3c5dc2f0bfe4f1aadb0e586fc
praveen1517-git <praveendsp4503@gmail.com>
2026-01-06 11:14:39 +0530

    homework

---
+++ Day 15+++/MovieArray.java
@@ -0,0 +1,11 @@
+class MovieArray
+{
+	public static void main(String[]args)
+	{
+		String movie[]={"Vidamuyarchi","Dragon","Coolie","Good Bad Ugly"};
+			for(int i=0;i< movie.length;i++)
+			{
+				System.out.println("Movie :" +movie[i]); 
+			}
+	}
+}
---
+++ Day 15+++/VowelLetterArray.java
@@ -0,0 +1,11 @@
+class VowelLetterArray
+{
+	public static void main(String[]args)
+	{
+		char vowelletter[]={'a','e','i','o','u'};
+			for(int i=0;i< vowelletter.length;i++)
+			{
+				System.out.println("vowel letter :" + vowelletter[i]); 
+			}
+	}
+}
