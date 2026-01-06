aa6283f17903f9f829b9b60cfadbe2372289dd86
praveen1517-git <praveendsp4503@gmail.com>
2026-01-06 12:20:24 +0530

    homework

---
+++ Day 15-+/DesignationArray.java
@@ -0,0 +1,15 @@
+class DesignationArray
+{
+	public static void main(String[]args)
+	{
+		String designation[]={"Software Engineer",
+							  "Data Analyst",
+							  "ProjectManager",
+							  "HR Manager", 
+							  "UI/UX Designer"};
+			for(int i=0;i< designation.length;i++)
+			{
+				System.out.println("Designation :" +designation[i]); 
+			}
+	}
+}
---
+++ Day 15-+/UnitsArray.java
@@ -0,0 +1,11 @@
+class UnitsArray
+{
+	public static void main(String[]args)
+	{
+		double units[]={320.5,289.8,310.2,280.6,299.9};
+			for(int i=0;i< units.length;i++)
+			{
+				System.out.println("people units :" + units[i]); 
+			}
+	}
+}
